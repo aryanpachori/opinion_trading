7c780b47714c86997be44ed959d75cc7009c5c70
Aryan Pachori <aryanpachori03@gmail.com>
2024-10-20 21:43:24 +0530

    Update user.ts

--- backend/router/user.ts
+++ backend/router/user.ts
@@ -10,9 +10,39 @@ router.post("/create", async (req, res) => {
   const data = {
     responseId,
     userId,
+    type: "userCreation",
   };
   await engineQueue(data);
-  await redis.subscribe("userCreation",)
+
+  await redis.subscribe("userCreation", (data) => {
+    const parseData = JSON.parse(data);
+    if (parseData.responseId == responseId) {
+      redis.unsubscribe("userCreation");
+      return res.json({
+        message: `"user added successfully with id:"${userId}`,
+      });
+    } else {
+      return res.status(401).json({ message: "user failed to create" });
+    }
+  });
+});
+
+router.post("/recharge", async (req, res) => {
+  const { userId, balance } = req.body;
+  const responseId = createId();
+  const data = {
+    userId,
+    balance,
+    type: "userRecharge",
+  };
+  await engineQueue(data);
+  await redis.subscribe("userRecharge", (data) => {
+    const parseData = JSON.parse(data);
+    if (parseData.responseId == responseId) {
+    } else {
+      return res.status(401).json({ message: "user recharge failed" });
+    }
+  });
 });
 
 export default router;
