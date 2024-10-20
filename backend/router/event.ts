410227996e34f9738f8c1bc69aaad385d36c5608
Aryan Pachori <aryanpachori03@gmail.com>
2024-10-20 23:02:08 +0530

    Added userRouter

--- backend/router/event.ts
+++ backend/router/event.ts
@@ -1,5 +1,29 @@
-import { Router } from "express";
+// import { createId } from "@paralleldrive/cuid2";
+// import { Router } from "express";
+// import { engineQueue, redis } from "../services/redisClient";
 
-const router = Router();
+// const router = Router();
 
-export default router;
+// router.post("/initiate",async(req,res)=>{
+//     const{userId , eventId , side , price ,quantity} = req.body
+//     if(!userId || !eventId|| !side || !price || !quantity){
+//         res.status(401).json({message : "invalid information"})
+//         return
+//     }
+//     const responseId = createId()
+//     const data = {
+//         userId,
+//         eventId,
+//         side , 
+//         price,
+//         quantity,
+//         type : "initiateOrder"
+//     }
+//     await engineQueue(data);
+//     await redis.subscribe("initiateOrder",(data)=>{
+//         const parseData = JSON.parse(data)
+//         if(parseDa)
+//     })
+// })
+
+// export default router;
--- backend/router/user.ts
+++ backend/router/user.ts
@@ -22,6 +22,7 @@ router.post("/create", async (req, res) => {
         message: `"user added successfully with id:"${userId}`,
       });
     } else {
+      redis.unsubscribe("userCreation");
       return res.status(401).json({ message: "user failed to create" });
     }
   });
@@ -38,9 +39,34 @@ router.post("/recharge", async (req, res) => {
   await engineQueue(data);
   await redis.subscribe("userRecharge", (data) => {
     const parseData = JSON.parse(data);
-    if (parseData.responseId == responseId) {
+    if (parseData.responseId == responseId && parseData.status === "SUCCESS") {
+      redis.unsubscribe("userRecharge");
+      return res.json({ message: "success" });
     } else {
+      redis.unsubscribe("userRecharge");
       return res.status(401).json({ message: "user recharge failed" });
+    }
+  });
+});
+
+router.post("/balance", async (req, res) => {
+  const { userId } = req.body;
+  const responseId = createId();
+
+  const data = {
+    userId,
+    responseId,
+    type: "userBalance",
+  };
+  await engineQueue(data);
+  await redis.subscribe("userBalance", (data) => {
+    const parseData = JSON.parse(data);
+    if (parseData.responseId == responseId && parseData.status === "SUCCESS") {
+      redis.unsubscribe("userBalance");
+      return res.json(`user balance: ${parseData.balance}`);
+    } else {
+      redis.unsubscribe("userBalance");
+      return res.json("error fetching the balance");
     }
   });
 });
