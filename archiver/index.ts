22f8b13bdc3d8097d131239344da91284d72f380
Aryan Pachori <aryanpachori03@gmail.com>
2024-11-10 21:19:04 +0530

    Update index.ts

--- archiver/index.ts
+++ archiver/index.ts
@@ -37,14 +37,26 @@ async function startArchiver() {
       const streamData = message[0];
       if (streamData && streamData.messages.length > 0) {
         const messages = streamData.messages;
-        
+
         for (const { id, message } of messages) {
           const messageData = JSON.parse(message.data);
 
           if (message.type == "order_creation") {
             console.log(message.type, message.data);
-            const order = await prisma.order.create({
-              data: {
+            const order = await prisma.order.upsert({
+              where: {
+                id: messageData.id,
+              },
+              update: {
+                userId: messageData.userId,
+                price: messageData.price,
+                Quantity: messageData.quantity,
+                type: messageData.type,
+                status: messageData.status,
+                eventId: messageData.eventId,
+                Side: messageData.side,
+              },
+              create: {
                 id: messageData.id,
                 userId: messageData.userId,
                 price: messageData.price,
@@ -56,11 +68,24 @@ async function startArchiver() {
               },
             });
             console.log(order);
-
           } else if (message.type == "trade") {
             console.log("TRADE", message.type, message.data);
-            const trade = await prisma.trade.create({
-              data: {
+            const trade = await prisma.trade.upsert({
+              where: {
+                id: messageData.id,
+              },
+              update: {
+                eventId: messageData.eventId,
+                sellerId: messageData.sellerId,
+                sellerOrderId: messageData.sellerOrder_id,
+                buyerOrderId: messageData.buyerOrder_id,
+                sellQty: messageData.sell_qty,
+                buyerId: messageData.buyerId,
+                buyQty: messageData.buy_qty,
+                buyPrice: messageData.Buyprice,
+                sellPrice: messageData.Sellprice,
+              },
+              create: {
                 id: messageData.id,
                 eventId: messageData.eventId,
                 sellerId: messageData.sellerId,
@@ -74,7 +99,6 @@ async function startArchiver() {
               },
             });
             console.log(trade);
-
           } else if (message.type == "order_update") {
             console.log("order_update", message.data);
             const order = await prisma.order.update({
@@ -82,7 +106,6 @@ async function startArchiver() {
               data: { type: messageData.type },
             });
             console.log(order);
-
           } else if (message.type == "order_exit") {
             console.log("order_exit", message.data);
             const order = await prisma.order.update({
@@ -99,4 +122,3 @@ async function startArchiver() {
     }
   }
 }
-
